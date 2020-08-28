89b7456529d157cf6620a57004e9ba0759190a60
tom <thomas.cassagne20@gmail.com>
2020-08-29 00:30:31 +0200

    first commit

---
+++ creation_table.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE IF NOT EXISTS "country"
+(
+    country_name VARCHAR PRIMARY KEY,
+    pop INTEGER,
+    density INTEGER,
+    land_area INTEGER,
+    medium_age INTEGER,
+    urban_pop VARCHAR
+);
