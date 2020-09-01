cbea2cc417a04517232301761642f85893a14766
tom <thomas.cassagne20@gmail.com>
2020-09-01 21:16:56 +0200

    commit

--- procedure/procedure.sql
+++ procedure/procedure.sql
@@ -9,6 +9,3 @@ BEGIN
 INSERT INTO table_country(country,pop,density) VALUES (pays, random() * 10000, random() * 10000);
 END;
 $$;
-
-CREATE TRIGGER data_upload BEFORE INSERT OR UPDATE ON table_country      
-FOR EACH ROW EXECUTE PROCEDURE data_upload();
